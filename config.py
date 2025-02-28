d4e8682600e785931be55a294cea6e3b2e9eaa21
Blake DeHaas <realblakedehaas@gmail.com>
2025-02-27 17:05:34 -0700

    Updated instructions

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ total_hours = 72
 #    each value is the percentage of the total hours to be allocated to that task.
 #    Ensure that the sum of all percentages equals 100.
 speedtype_percentage_distribution = {
-    "Phase C F IXPE OPS S 13300081": 20,
-    "TSIS-1 Follow On Ground Systems 13021467": 80,
+    "Phase C F OPS": 20,
+    "Follow On Ground Systems": 80,
 }
 
 # 3. Length of Workday
@@ -30,17 +30,17 @@ length_of_workday = 8
 # Example:
 # If the total number of hours you worked for this timesheet is 72, then set total_hours to 72 
 # If the length of your workday is 8 hours, then set length_of_workday to 8
-# If you are distributing your hours to the speedtype "Phase C F IXPE OPS S 13300081" at 20% and "TSIS-1 Follow On Ground Systems 13021467" at 80%,
+# If you are distributing your hours to the speedtype "Phase C F OPS" at 20% and "Follow On Ground Systems" at 80%,
 # then add the following entries to the speedtype_percentage_distribution dictionary:
-# "Phase C F IXPE OPS S 13300081": 20,
-# "TSIS-1 Follow On Ground Systems 13021467": 80
+# "Phase C F OPS": 20,
+# "Follow On Ground Systems": 80
 
 # After configuring the timesheet, run the TimesheetCalculator.py script to calculate the distribution of hours using this command:
 # python3 TimesheetCalculator.py
 
 # Your speedtype will be distributed as follows:
 # Total Hours: 72
-# Phase C F IXPE OPS S 13300081: 14 hours and 24 minutes (20%)
+# Phase C OPS: 14 hours and 24 minutes (20%)
 #     -> 1 full 8-hour workdays, 6 hours and 24 minutes remainder
-# TSIS-1 Follow On Ground Systems 13021467: 57 hours and 36 minutes (80%)
+# Follow On Ground Systems: 57 hours and 36 minutes (80%)
 #     -> 7 full 8-hour workdays, 1 hours and 36 minutes remainder
